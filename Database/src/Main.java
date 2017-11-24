44a84ee7bb45701ba27512912ab50884d7b6392e
Roey Ashkenazy <roeyash5@gmail.com>
2017-11-24 18:53:01 +0200

    #2 Added Technion icon and various fixes.

--- Database/src/Main.java
+++ Database/src/Main.java
@@ -2,9 +2,11 @@ package src;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.NodeOrientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -13,8 +15,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-        primaryStage.setTitle("Semester Picking");
-        primaryStage.setScene(new Scene(root,800,500));
+        primaryStage.setTitle("בחירת סמסטר");
+        Scene scene = new Scene(root,800,500);
+        primaryStage.getIcons().add(new Image("resources/Technion-logo2.png"));
+        //scene.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
