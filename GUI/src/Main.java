9c91c4df9683e84647c91c1728ff48e8bb9bea34
Roey Ashkenazy <roeyash5@gmail.com>
2017-12-01 17:54:04 +0200

    #1 #2 #4 added inter-screen communication.

--- GUI/src/Main.java
+++ GUI/src/Main.java
@@ -1,19 +1,24 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.NodeOrientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import Screens.SemesterPicking.SemesterPickingController;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        root.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("Screens/SemesterPicking/semesterPicking.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Screens/SemesterPicking/semesterPicking.fxml"));
+        root.getStylesheets().add(getClass().getResource("Screens/SemesterPicking/semesterPicking.css").toExternalForm());
+
+        myLoader.setController(new SemesterPickingController());
         primaryStage.setTitle("בחירת סמסטר");
         Scene scene = new Scene(root,800,500);
         primaryStage.getIcons().add(new Image("resources/Technion-logo2.png"));
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- GUI/src/Screens/SemesterPicking/SemesterPickingController.java
+++ GUI/src/Screens/SemesterPicking/SemesterPickingController.java
@@ -1,22 +1,28 @@
-package src;
+package Screens.SemesterPicking;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.NodeOrientation;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.Calendar;
 import java.util.ResourceBundle;
 
-import static src.SEMESTER.SEMESTER_A;
-import static src.SEMESTER.SEMESTER_B;
-import static src.SEMESTER.SEMESTER_K;
+import static Screens.SemesterPicking.SEMESTER.SEMESTER_A;
+import static Screens.SemesterPicking.SEMESTER.SEMESTER_B;
+import static Screens.SemesterPicking.SEMESTER.SEMESTER_K;
 
 enum SEMESTER{ SEMESTER_A, SEMESTER_B, SEMESTER_K }
-public class Controller implements Initializable {
+public class SemesterPickingController implements Initializable {
+
     public VBox topBar;
     @FXML
     JFXComboBox<String> yearChoice;
@@ -38,23 +44,40 @@ public class Controller implements Initializable {
             yearChoice.getItems().add(i.toString()); 
         }
     }
-
-    private void createAlert(String text, String title){
+    private void createAlert(String text){
         Alert alert = new Alert(Alert.AlertType.ERROR, text, ButtonType.OK);
         alert.setHeaderText("");
-        alert.setTitle(title);
+        alert.setTitle("שגיאה");
         alert.getDialogPane().setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
         alert.showAndWait();
+    }
+    public void onPrevButtonClick(){
+        Stage stage = new Stage();
+        stage.setTitle("מסך כניסה");
+        stage.getIcons().add(new Image("resources/Technion-logo2.png"));
+
+        Pane myPane = null;
+        try {
+            myPane = FXMLLoader.load(getClass().getResource("/Screens/Login/login.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = null;
+        if (myPane != null) {
+            scene = new Scene(myPane);
+        }
+        stage.setScene(scene);
+        stage.show();
     }
     public void onNextButtonClick(){
         wantedYear = yearChoice.getValue();
         if (wantedYear == null) {
-            createAlert("אנא בחר/י שנת לימודים.","שגיאה");
+            createAlert("אנא בחר/י שנת לימודים.");
             return;
         }
         RadioButton pickedButton = (RadioButton)semesterToggleGroup.getSelectedToggle();
         if(pickedButton == null){
-            createAlert("אנא בחר/י סמסטר.","שגיאה");
+            createAlert("אנא בחר/י סמסטר.");
             return;
         }
         if(radioButtonA == pickedButton){
