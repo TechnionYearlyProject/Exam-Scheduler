a6c726f3b482fe606bffce2015b3b31561e0b1dd
Raphael Azoulay <raphalaz@campus.technion.ac.il>
2017-12-10 10:27:42 +0200

    Added codecov integration + minimal test

---
+++ Test/db/CourseTest.java
@@ -0,0 +1,25 @@
+package db;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseTest {
+
+    public static Course c;
+
+    @Before
+    public void initCourse() {
+        c = new Course(123456, "abcdef");
+    }
+
+    @Test
+    public void setStudyProgramTest() {
+        c.setStudyProgram("abc", 1);
+        c.setStudyProgram("def", 1);
+        assertEquals(1, c.getStudyProgramSemester("abc"));
+        assertEquals(1, c.getStudyProgramSemester("def"));
+        assertEquals(0, c.getStudyProgramSemester("abcdef"));
+    }
+}
