e993b98169cc8c365fb62afbe7f2d6817491219e
Roey Ashkenazy <roeyash5@gmail.com>
2017-12-11 17:28:18 +0200

    #4 improved readablity of calendar and allowed multiple exams on the same day

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,26 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import GUI.Screens.SemesterPicking.SemesterPickingController;
+import Screens.SemesterPicking.SemesterPickingController;
+
+import java.io.File;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("GUI/Screens/SemesterPicking/semesterPicking.fxml"));
+        FXMLLoader myLoader = new FXMLLoader(getClass().getResource("/Screens/SemesterPicking/semesterPicking.fxml"));
         SemesterPickingController controller = new SemesterPickingController();
         controller.setPrevStage(primaryStage);
         myLoader.setController(controller);
         Parent root = myLoader.load();
-        //Parent root = myLoader.load(getClass().getResource("Screens/SemesterPicking/semesterPicking.fxml"));
-        root.getStylesheets().add(getClass().getResource("GUI/Screens/SemesterPicking/semesterPicking.css").toExternalForm());
+        //Parent root = myLoader.load(getClass().getResource("GUI/Screens/SemesterPicking/semesterPicking.fxml"));
+        root.getStylesheets().add(getClass().getResource("/Screens/SemesterPicking/semesterPicking.css").toExternalForm());
 
 
         primaryStage.setTitle("בחירת סמסטר");
         Scene scene = new Scene(root,800,500);
-        primaryStage.getIcons().add(new Image("GUI/resources/Technion-logo2.png"));
+        primaryStage.getIcons().add(new Image(new File("src/GUI/resources/Technion-logo2.png").toURI().toString()));
         SemesterPickingController control = myLoader.getController();
         control.setPrevStage(primaryStage);
         primaryStage.setScene(scene);
