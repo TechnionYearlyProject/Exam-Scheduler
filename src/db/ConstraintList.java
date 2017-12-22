a273c4e48a829b8154c939742d181b85e5388038
ucfBader <yose981@gmail.com>
2017-12-23 01:05:50 +0200

    remove static class and update relevant files accordingly (Database, Schedule).

--- src/db/ConstraintList.java
+++ src/db/ConstraintList.java
@@ -8,6 +8,35 @@ import java.util.*;
 public class ConstraintList {
     public Map<Integer, List<Constraint>> constraints;
 
+    static class Constraint implements Comparable<Constraint>{
+        public Calendar start;
+        public Calendar end;
+
+        public Constraint(Calendar start, Calendar end) {
+            this.start = (Calendar) start.clone();
+            this.end = (Calendar) end.clone();
+        }
+
+        @Override
+        public int compareTo(Constraint other) {
+            if (start.before(other.start)) {
+                return -1;
+            }
+            if (start.after(other.start)) {
+                return 1;
+            }
+            return 0;
+        }
+
+        public Calendar getStart(){
+            return (Calendar)start.clone();
+        }
+
+        public Calendar getEnd(){
+            return (Calendar)end.clone();
+        }
+    }
+
     public ConstraintList() {
         constraints = new HashMap<>();
     }
