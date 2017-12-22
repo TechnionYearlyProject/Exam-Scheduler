77bc39e5f60a201baebb09d14e6621eb632d1dd6
ucfBader <yose981@gmail.com>
2017-12-22 23:52:12 +0200

    Logic.Course class: in this class we saves all the relevant data to the algorithm. this class is based on db.Course class.
    few changes at the class data structures.

--- src/Logic/Course.java
+++ src/Logic/Course.java
@@ -1,66 +1,76 @@
 package Logic;
 
+import db.Constraint;
+
 import java.util.ArrayList;
-import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Course {
     private int courseID;
     private String courseName;
-    private ArrayList<Course> conflictCourses;
+    private Map<Integer,String> conflictCourses;
     private boolean isLast, isFirst, isRequired;//TODO: maybe change to other name
     private int daysBefore;
-//    private ArrayList<Constraint> constraints;
-//    static class Constraint {
-//        Calendar start;
-//        Calendar end;
-//
-//        public Constraint(Calendar start, Calendar end) {
-//            this.start = (Calendar) start.clone();
-//            this.end = (Calendar) end.clone();
-//        }
-//    }
+    //private int points;
+    private ArrayList<Constraint> constraints;
 
-    static void setCoursesAsConflicts(Course c1, Course c2){
-        c1.addConflictCourse(c2);
-        c2.addConflictCourse(c1);
-    }
-
-    Course(String courseName,int courseID, boolean isLast, boolean isFirst, int daysBefore, boolean isRequired) {
-        conflictCourses = new ArrayList<>();
-        this.courseID = courseID;
-        this.courseName = courseName;
-        this.isFirst = isFirst;
-        this.isLast = isLast;
-        this.daysBefore = daysBefore;
-        this.isRequired = isRequired;
+    /*
+     * Logic.Course copy c'tor performing deepCopy.
+     */
+    public Course(Course o) {
+        courseID = o.courseID;
+        courseName = o.courseName;
+        isRequired = o.isRequired;
+        isLast = o.isLast;
+        isFirst = o.isFirst;
+        daysBefore = o.daysBefore;
+        conflictCourses = new HashMap<>();
+        constraints = new ArrayList<>();
+        constraints.addAll(o.getConstraints());
+        conflictCourses = o.getConflictCourses();
     }
 
     Course(String courseName, int courseID, boolean isRequired){
-        conflictCourses = new ArrayList<>();
+        conflictCourses = new HashMap<>();
+        constraints = new ArrayList<>();
         this.courseID = courseID;
         this.courseName = courseName;
         this.isLast = false;
         this.isFirst = false;
         this.isRequired = isRequired;
-        this.daysBefore = 4;
+        this.daysBefore = 4;//TODO: what is the real value?
     }
 
-    void addConflictCourse(Course c){
-        if(conflictCourses.contains(c)){
+    void addConstraint(Constraint c){
+        if(constraints.contains(c)){
             return;
         }
-        conflictCourses.add(c);
+        constraints.add(new Constraint(c.getStart(),c.getEnd()));
     }
 
-    void removeConflictCourse(Course c){
+    void removeConstraint(Constraint c){
+        constraints.remove(c);
+    }
+
+    void addConflictCourse(int courseID, String courseName){
+        if(this.courseID == courseID){
+            return;
+        }
+        if(conflictCourses.containsKey(courseID)){
+            return;
+        }
+        conflictCourses.put(courseID,courseName);
+    }
+
+    void removeConflictCourse(int c){
         conflictCourses.remove(c);
     }
 
-    void addConflictCourses(List<Course> courses){
-        for (Course c:courses) {
-            addConflictCourse(c);
+    void addConflictCourses(List<db.Course> courses){
+        for (db.Course c: courses) {
+            addConflictCourse(c.id,c.name);
         }
     }
 
@@ -79,26 +89,34 @@ public class Course {
         return daysBefore;
     }
 
-    void setCourseAsLast(){
-        this.isLast = true;
+    void setAsLast(boolean isLast){
+        this.isLast = isLast;
     }
     boolean isLast(){
         return isLast;
     }
 
-    void setCourseAsFirst(){
-        this.isFirst = true;
+    void setAsFirst(boolean isFirst){
+        this.isFirst = isFirst;
     }
     boolean isFirst(){
         return isFirst;
     }
 
-    void setCourseAsRequired(){this.isRequired = true;}
+    void setAsRequired(boolean isRequired){this.isRequired = isRequired;}
     boolean isRequired(){return isRequired;}
 
-    ArrayList<Course> getConflictCourses(){
-        return conflictCourses;
+    Map<Integer,String> getConflictCourses(){
+       return new HashMap<>(conflictCourses);
+
     }
+
+    ArrayList<Constraint> getConstraints(){
+        ArrayList<Constraint> cconstraints = new ArrayList<>();
+        cconstraints.addAll(this.constraints);
+        return cconstraints;
+    }
+
     int getNumOfConflictCourses(){
         return conflictCourses.size();
     }
