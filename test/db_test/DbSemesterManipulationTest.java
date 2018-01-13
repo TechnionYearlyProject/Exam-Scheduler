bc93c98899315ba1757816b11e5c09cd89e73dd6
rephaelaz <raphalaz@campus.technion.ac.il>
2018-01-13 22:48:19 +0200

    #68 Added test for Course registration to programs

--- test/db_test/DbSemesterManipulationTest.java
+++ test/db_test/DbSemesterManipulationTest.java
@@ -3,7 +3,9 @@ package db_test;
 import db.Database;
 import db.Semester;
 import db.exception.CourseAlreadyExist;
+import db.exception.CourseUnknown;
 import db.exception.StudyProgramAlreadyExist;
+import db.exception.StudyProgramUnknown;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -62,13 +64,6 @@ public class DbSemesterManipulationTest {
         try {
             semester = db.createSemester(2017, "winter");
             assertNotNull(semester);
-            semester.addStudyProgram("There is no spoon");
-            semester.addStudyProgram("Hasta la vista baby");
-            semester.addStudyProgram("Zed is dead");
-        } catch (Exception e) {
-            fail("Unexpected exception: " + e.toString());
-        }
-        try {
             semester.addCourse(123, "North By Northwest", 5.0);
             semester.addCourse(456, "Gone with the wind", 3.0);
             semester.addCourse(111, "Kill Bill", 3.0);
@@ -94,6 +89,67 @@ public class DbSemesterManipulationTest {
             semester.removeCourse(123); // No effect
             semester.removeCourse(111); // No effect
             assertEquals(2, semester.getCourseCollection().size());
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+    }
+
+    @Test
+    public void CourseRegisterTest() {
+        db.baseDirectory = baseDir + db.sep + "empty_db";
+        Semester semester = null;
+        try {
+            semester = db.createSemester(2017, "winter");
+            assertNotNull(semester);
+            semester.addStudyProgram("There is no spoon");
+            semester.addStudyProgram("I see dead people");
+            semester.addStudyProgram("Hasta la vista baby");
+            semester.addStudyProgram("Zed is dead");
+            semester.addCourse(123, "North By Northwest", 5.0);
+            semester.addCourse(456, "Gone with the wind", 3.0);
+            semester.addCourse(111, "Kill Bill", 3.0);
+            semester.addCourse(7777, "Kill Bill", 3.0);
+            semester.addCourse(1234, "The good, the bad and the ugly", 7.0);
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+        try {
+            semester.registerCourse(123, "There is no spoon", 1);
+            semester.registerCourse(123, "I see dead people", 1);
+            semester.registerCourse(123, "Hasta la vista baby", 3);
+            semester.registerCourse(123, "Zed is dead", 2);
+            semester.registerCourse(456, "There is no spoon", 4);
+            semester.registerCourse(456, "I see dead people", 4);
+            semester.registerCourse(456, "Hasta la vista baby", 4);
+            semester.registerCourse(456, "Zed is dead", 4);
+            semester.registerCourse(111, "There is no spoon", 1);
+            semester.registerCourse(111, "I see dead people", 1);
+            semester.registerCourse(111, "Hasta la vista baby", 1);
+            semester.registerCourse(111, "Zed is dead", 1);
+            semester.registerCourse(7777, "There is no spoon", 3);
+            semester.registerCourse(7777, "I see dead people", 5);
+            semester.registerCourse(7777, "Hasta la vista baby", 7);
+            semester.registerCourse(7777, "Zed is dead", 99);
+            semester.registerCourse(1234, "There is no spoon", 4);
+            semester.registerCourse(1234, "I see dead people", 3);
+            semester.registerCourse(1234, "Hasta la vista baby", 2);
+            semester.registerCourse(1234, "Zed is dead", 123);
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+        try {
+            semester.registerCourse(0, "There is no spoon", 2);
+            fail("Should have thrown CourseUnknown exception");
+        } catch (CourseUnknown e) {
+            // Expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+        try {
+            semester.registerCourse(123, "Try again my dear", 2);
+            fail("Should have thrown StudyProgramUnknown exception");
+        } catch (StudyProgramUnknown e) {
+            // Expected
         } catch (Exception e) {
             fail("Unexpected exception: " + e.toString());
         }
