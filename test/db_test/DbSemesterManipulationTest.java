89563fe4048cbd8148c9798cae2a05009ffbbf68
rephaelaz <raphalaz@campus.technion.ac.il>
2018-01-13 21:35:04 +0200

    #68 Added test for Course add/remove

--- test/db_test/DbSemesterManipulationTest.java
+++ test/db_test/DbSemesterManipulationTest.java
@@ -2,6 +2,7 @@ package db_test;
 
 import db.Database;
 import db.Semester;
+import db.exception.CourseAlreadyExist;
 import db.exception.StudyProgramAlreadyExist;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,7 +22,7 @@ public class DbSemesterManipulationTest {
     }
 
     @Test
-    public void addRemoveSemesterTest() {
+    public void StudyProgramTest() {
         db.baseDirectory = baseDir + db.sep + "empty_db";
         Semester semester = null;
         try {
@@ -37,6 +38,7 @@ public class DbSemesterManipulationTest {
         }
         try {
             semester.addStudyProgram("There is no spoon");
+            fail("Should have thrown StudyProgramAlreadyExist exception");
         } catch (StudyProgramAlreadyExist e) {
             // Nothing to do
         } catch (Exception e) {
@@ -46,11 +48,52 @@ public class DbSemesterManipulationTest {
             semester.removeStudyProgram("I see dead people");
             semester.removeStudyProgram("Zed is dead");
             assertEquals(2, semester.getStudyProgramCollection().size());
+            semester.removeStudyProgram("I see dead people"); // No effect
+            assertEquals(2, semester.getStudyProgramCollection().size());
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+    }
+
+    @Test
+    public void CourseTest() {
+        db.baseDirectory = baseDir + db.sep + "empty_db";
+        Semester semester = null;
+        try {
+            semester = db.createSemester(2017, "winter");
+            assertNotNull(semester);
+            semester.addStudyProgram("There is no spoon");
+            semester.addStudyProgram("Hasta la vista baby");
+            semester.addStudyProgram("Zed is dead");
         } catch (Exception e) {
             fail("Unexpected exception: " + e.toString());
         }
         try {
-            semester.removeStudyProgram("There is no spoon");
+            semester.addCourse(123, "North By Northwest", 5.0);
+            semester.addCourse(456, "Gone with the wind", 3.0);
+            semester.addCourse(111, "Kill Bill", 3.0);
+            semester.addCourse(7777, "Kill Bill", 3.0); // Course is defined by ID
+            semester.addCourse(1234, "The good, the bad and the ugly", 7.0);
+            assertEquals(5, semester.getCourseCollection().size());
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+        try {
+            semester.addCourse(123, "Wowowow guy calm down", 99.99);
+            fail("Should have thrown CourseAlreadyExist exception");
+        } catch (CourseAlreadyExist e) {
+            // Expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.toString());
+        }
+        try {
+            semester.removeCourse(123);
+            semester.removeCourse(7777);
+            semester.removeCourse(111);
+            assertEquals(2, semester.getCourseCollection().size());
+            semester.removeCourse(123); // No effect
+            semester.removeCourse(111); // No effect
+            assertEquals(2, semester.getCourseCollection().size());
         } catch (Exception e) {
             fail("Unexpected exception: " + e.toString());
         }
