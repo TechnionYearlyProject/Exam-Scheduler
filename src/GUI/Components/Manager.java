e144540061cd4e46d982def272dbd05c5c461b43
Roey Ashkenazy <roeyash5@gmail.com>
2018-01-14 18:35:26 +0200

    #74 #54 added search function for courses

--- src/GUI/Components/Manager.java
+++ src/GUI/Components/Manager.java
@@ -1,6 +1,9 @@
 package GUI.Components;
+import db.Course;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.geometry.NodeOrientation;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
@@ -16,11 +19,34 @@ public class Manager extends HBox {
     private Moed A;
     private Moed B;
     private CoursesTable courses;
+    private FilteredList<Item> getData() {
+        ObservableList<Item> items = FXCollections.observableArrayList();
+        items.add(new Item(new Course(234141, "קומבינטוריקה למדעי המחשב", 3)));
+        items.add(new Item(new Course(234123, "מערכות הפעלה", 4)));
+        items.add(new Item(new Course(236353, "אוטומטים ושפות פורמליות", 3)));
+        items.add(new Item(new Course(104032, "חשבון אינפיניטסימלי 2מ'", 5)));
+        return new FilteredList<>(items);
+    }
     public Manager() {
         A = new Moed("מועד א'");
         B = new Moed("מועד ב'");
         courses = new CoursesTable();
-        this.getChildren().addAll(B,A,courses);
+        FilteredList<Item> filteredList = getData();
+
+        TextField filterInput = new TextField();
+        filterInput.setPromptText("חפש קורס...");
+        filterInput.textProperty().addListener(obs->{
+            String filter = filterInput.getText();
+            if(filter == null || filter.length() == 0) {
+                filteredList.setPredicate(s -> true);
+            }
+            else {
+                filteredList.setPredicate(s -> s.name.contains(filter));
+            }
+        });
+        filterInput.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        courses.setItems(filteredList);
+        this.getChildren().addAll(B,A,new VBox(filterInput, courses));
         this.setAlignment(Pos.TOP_RIGHT);
         this.setSpacing(20);
     }
