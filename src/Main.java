e2d082704e3dd655c71a9d44cec5c5f2fd09951d
dorbartov <dorbartov@gmail.com>
2018-01-18 21:16:53 +0200

    issue #116

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
-
+import GUI.Components.AlertType;
 import GUI.Components.Wrapper;
-import Output.CalendarFileWriter;
-import Output.XMLFileWriter;
+import GUI.Components.AlertBox;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
-
-import java.util.Calendar;
 
 public class Main extends Application {
 
@@ -18,15 +15,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Scene main_window = new Scene(new Wrapper());
-        main_window.setOnDragOver(event -> {
-            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
-            event.consume();
-        });
-        primaryStage.setTitle("Exam Scheduler");
-        primaryStage.setScene(main_window);
-        primaryStage.getIcons().add(new Image("/app_icon.png"));
-        //primaryStage.setResizable(false);
-        primaryStage.show();
+        try {
+            Scene main_window = new Scene(new Wrapper());
+            main_window.setOnDragOver(event -> {
+                event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                event.consume();
+            });
+            primaryStage.setTitle("Exam Scheduler");
+            primaryStage.setScene(main_window);
+            primaryStage.getIcons().add(new Image("/app_icon.png"));
+            primaryStage.setResizable(false);
+            primaryStage.setMaxHeight(927);
+            primaryStage.setMinHeight(927);
+            primaryStage.setMaxWidth(1691);
+            primaryStage.setMinWidth(1691);
+            primaryStage.show();
+        } catch (Exception e) {
+            new AlertBox(AlertType.ERROR,"התוכנית נתקלה בשגיאה ולכן תיסגר.",null,true);
+            System.exit(1);
+        }
     }
 }
