3a251bdca08fb7db7d545271d8c7424c63b120b3
ucfBader <yose981@gmail.com>
2018-01-19 19:32:23 +0200

    #128 excel file format. (csv) now we can open it in excel without manual import.

---
+++ src/Output/ExcelFileWriter.java
@@ -0,0 +1,83 @@
+package Output;
+import Logic.Course;
+import Logic.CourseLoader;
+import Logic.Day;
+import Output.Exceptions.ErrorOpeningFile;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/*
+ * @author: ucfBader
+ * @date: 19/01/2018.
+ * in this class we write the schedule as Excel file in the following format: Date, CourseID, CourseName.
+ * the user will be able to open the final schedule as Excel file containing all relevant data.
+ */
+public class ExcelFileWriter implements IFileWriter {
+    /*
+     * @param lst: the schedule days, each day contains list of courses scheduled in that day.
+     * calculates number of scheduled courses.
+     */
+
+
+    /*
+     * @param: filename: the wanted filename (output.xml) as default.
+     * @lst: the schedule days, each day contains list of courses scheduled in that day.
+     * @cL: courseLoader class instance, in order to get courseName.
+     */
+    @Override
+    public void write(String fileName, List<Day> lst, CourseLoader cL) throws ErrorOpeningFile {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet = workbook.createSheet("מועדי א");
+        int numOfExams = getNumOfDaysScheduled(lst);
+        String[][] exams = new String[numOfExams+1][3];
+        exams[0][0] = "תאריך";
+        exams[0][1] ="מספר הקורס";
+        exams[0][2] = "שם הקורס";
+        CellStyle style = workbook.createCellStyle();
+        style.setAlignment(HorizontalAlignment.RIGHT);
+
+
+        int row = 1;
+        int col = 0;
+        for (Day d: lst) {
+            StringBuilder sb = new StringBuilder();
+            LocalDate localDate = d.getDate();
+            sb.append(String.format("%02d",localDate.getDayOfMonth()));
+            sb.append("/");
+            sb.append(String.format("%02d",localDate.getMonthValue()));
+            sb.append("/");
+            sb.append(localDate.getYear());
+            for (Integer key: d.getCoursesScheduledToTheDay()) {
+                col = 0;
+                exams[row][col++] = sb.toString();
+                exams[row][col++] = String.format("%06d",key);
+                Course c = cL.getCourse(key);
+                exams[row++][col] = c.getCourseName();
+            }
+        }
+
+        int rowNum = 0;
+        for (String[] exam: exams) {
+            Row r = sheet.createRow(rowNum);
+            int colNum = 0;
+            for (String field : exam) {
+                Cell cell = r.createCell(colNum);
+                if (field != null) {
+                    cell.setCellValue(field);
+                    style.setAlignment(HorizontalAlignment.RIGHT);//setting each cell alignment.
+                    cell.setCellStyle(style);
+                }
+                colNum++;
+            }
+            rowNum++;
+        }
+        writeFile(fileName, workbook);
+    }
+}
--- src/Output/IFileWriter.java
+++ src/Output/IFileWriter.java
@@ -3,10 +3,35 @@ package Output;
 import Logic.CourseLoader;
 import Logic.Day;
 import Output.Exceptions.ErrorOpeningFile;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.time.format.DateTimeFormatter;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
+/*
+ * @author: ucfBader.
+ * each file writer can implement this interface which supplies two default methods, for writing file formats.
+ */
 public interface IFileWriter {
+    /*
+     * each file writer should implement this method, defining file format.
+     */
     public void write(String fileName, List<Day> lst, CourseLoader cL) throws ErrorOpeningFile;
+    default int getNumOfDaysScheduled(List<Day> lst){
+        int exams = 0;
+        for (Day d: lst) {
+            exams+=d.getNumOfCourses();
+        }
+        return exams;
+    }
+    default void writeFile(String fileName, XSSFWorkbook workbook) {
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
