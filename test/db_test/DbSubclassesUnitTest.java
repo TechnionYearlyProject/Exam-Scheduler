014e602a2bd7fc595fd5b05836d12c166754f373
rephaelaz <raphalaz@campus.technion.ac.il>
2018-01-19 13:28:43 +0200

    #68 Added unit test for Constraint

--- test/db_test/DbSubclassesUnitTest.java
+++ test/db_test/DbSubclassesUnitTest.java
@@ -1,8 +1,11 @@
 package db_test;
 
+import db.Constraint;
 import db.Course;
 import Logic.Exceptions.IllegalDaysBefore;
 import org.junit.Test;
+
+import java.time.LocalDate;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
@@ -40,5 +43,17 @@ public class DbSubclassesUnitTest {
         Course course = new Course(101, "Edmond Dantes", 5.0, 4,
                 false, false, true, true);
         course.setDaysBefore(-1);
+    }
+
+    @Test
+    public void ConstraintUnitTest() {
+        Constraint c1 = new Constraint(LocalDate.parse("2017-01-01"), false);
+        Constraint c2 = new Constraint(LocalDate.parse("2017-01-07"));
+        Constraint c3 = new Constraint(c1);
+        Constraint c4 = new Constraint(LocalDate.parse("2018-01-01"));
+        assertTrue(c1.equals(c3));
+        assertEquals(-1, c1.compareTo(c2));
+        assertEquals(1, c4.compareTo(c2));
+        assertEquals(0, c1.compareTo(c3));
     }
 }
