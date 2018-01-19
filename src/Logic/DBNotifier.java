2eba4b92f345286d8f6539cdc909915f1356eed3
ucfBader <yose981@gmail.com>
2018-01-20 00:55:53 +0200

    #64 when the user clicks save, we need to update our db if the data was changed. added and removed courses will be written accordingly. saving all constraints.

---
+++ src/Logic/DBNotifier.java
@@ -0,0 +1,64 @@
+package Logic;
+
+import db.Constraint;
+import db.Semester;
+import db.exception.*;
+import javafx.util.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class DBNotifier {
+    public void save(CourseLoader cL,Semester s,Schedule schedule){
+        Semester.Moed sm;
+        if(schedule == null){
+            sm = Semester.Moed.MOED_A;
+        } else {
+            sm = Semester.Moed.MOED_B;
+        }
+        List<Course> courses = cL.getSortedCourses();
+        for(Course c:courses){
+            updateDBCourse(c,s);
+            for (Constraint con: c.getConstraints()) {
+                try {
+                    s.addConstraint(c.getCourseID(),sm,con.date,con.forbidden);
+                } catch (UninitializedSchedule | DateOutOfSchedule | DuplicateConstraints | CourseUnknown uninitializedSchedule) {
+                    uninitializedSchedule.printStackTrace();
+                }
+            }
+        }
+
+        for (Course c: cL.removedCourses) {
+            s.removeCourse(c.getCourseID());
+        }
+    }
+
+    private void updateDBCourse(Course course, Semester s){
+        db.Course c;
+        try {
+            c = s.getCourse(course.getCourseID());
+        } catch (CourseUnknown courseUnknown) {
+            try {
+                s.addCourse(course.getCourseID(),course.getCourseName(),course.getCreditPoints(),
+                        course.getDaysBefore(),course.isFirst(),course.isLast(),course.isRequired(),course.hasExam());
+
+            } catch (CourseAlreadyExist | CourseFirstAndLast courseAlreadyExist) {
+                courseAlreadyExist.printStackTrace();
+            }
+            return;
+        }
+        c.hasExam = course.hasExam();
+        c.daysBefore = course.getDaysBefore();
+        c.isRequired = course.isRequired();
+        c.isFirst = course.isFirst();
+        c.isLast = course.isLast();
+        Map<String,Integer> m = new HashMap<>();
+        Set<Pair<String,Integer>> tmp = course.getPrograms();
+        for (Pair<String, Integer> p: tmp) {
+            m.put(p.getKey(),p.getValue());
+        }
+        c.programs = m;
+    }
+}
