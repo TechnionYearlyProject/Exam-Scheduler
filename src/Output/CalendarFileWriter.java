5ab6bd6552daccf71bf78472c2ffd3e7fe48d34e
ucfBader <yose981@gmail.com>
2018-01-19 23:56:45 +0200

    #130 fixed!

--- src/Output/CalendarFileWriter.java
+++ src/Output/CalendarFileWriter.java
@@ -7,8 +7,6 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
